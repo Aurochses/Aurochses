827e38958e50f679fa0f2811451076c126d82bc3
Vladimir Rodchenko <vladimir@rodchenko.by>
2018-12-22 17:33:55 +0300

    added Aurochses.Xunit info

--- data/dotNet.js
+++ data/dotNet.js
@@ -190,9 +190,22 @@ const dotNetGroups = [
         "items": [
             {
                 "name": "Aurochses.Xunit",
+                "description": "Aurochses.Xunit is a library for creating xUnit tests.",
                 "gitHubOwner": "Aurochses",
                 "gitHubRepository": "Aurochses.Xunit",
+                "azureDevOpsOrganization": "Aurochses",
+                "azureDevOpsProject": "Aurochses.GitHub",
+                "azureDevOpsBuildId": 372,
+                "azureDevOpsReleaseProjectId": "61cd8e26-670f-4d15-9b53-5e73a476a30f",
+                "azureDevOpsReleaseId": 2,
+                "azureDevOpsReleaseEnvironmentId": 2,
                 "nuGetPackage": "Aurochses.Xunit",
+                "nuGetPackageVersion": "2.1.0",
+                "nuGetPackageAuthors": "Aurochses",
+                "nuGetPackageCompany": "Aurochses",
+                "nuGetPackageCopyright": "Aurochses 2017",
+                "nuGetPackageIconUrl": "https://www.gravatar.com/avatar/cb5925a77e502401f95bf2738b123994?s=64",
+                "nuGetPackageTags": "Test Unit xUnit",
                 "info": "OK"
             },
             {
