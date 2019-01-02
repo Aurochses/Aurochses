29103526c3a2bcb128ee2fda6ec3f8c6beefcca3
Vladimir Rodchenko <vladimir@rodchenko.by>
2019-01-02 12:31:00 +0300

    added Aurochses.AspNetCore.Identity.SendGrid info

--- data/dotNet.js
+++ data/dotNet.js
@@ -134,10 +134,23 @@ const dotNetGroups = [
             },
             {
                 "name": "Aurochses.AspNetCore.Identity.SendGrid",
+                "description": "Aurochses.AspNetCore.Identity.SendGrid is a library for ASP.NET Core Identity with SendGrid.",
                 "gitHubOwner": "Aurochses",
                 "gitHubRepository": "Aurochses.AspNetCore.Identity.SendGrid",
+                "azureDevOpsOrganization": "Aurochses",
+                "azureDevOpsProject": "Aurochses.GitHub",
+                "azureDevOpsBuildId": 389,
+                "azureDevOpsReleaseProjectId": "61cd8e26-670f-4d15-9b53-5e73a476a30f",
+                "azureDevOpsReleaseId": 11,
+                "azureDevOpsReleaseEnvironmentId": 11,
                 "nuGetPackage": "Aurochses.AspNetCore.Identity.SendGrid",
-                "info": "move to 2.1.0"
+                "nuGetPackageVersion": "2.1.0",
+                "nuGetPackageAuthors": "Aurochses",
+                "nuGetPackageCompany": "Aurochses",
+                "nuGetPackageCopyright": "Aurochses 2017",
+                "nuGetPackageIconUrl": "https://www.gravatar.com/avatar/cb5925a77e502401f95bf2738b123994?s=64",
+                "nuGetPackageTags": "SendGrid Identity ASP.NET Core",
+                "info": "OK"
             },
             {
                 "name": "Aurochses.AspNetCore.Identity.Twilio",
